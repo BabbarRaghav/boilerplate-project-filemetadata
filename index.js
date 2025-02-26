6768a2f1909485e73d2236da1841f367efa7b4be
Raghav Babbar <89309326+BabbarRaghav@users.noreply.github.com>
2025-02-26 16:21:39 +0000

    final test 5

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
 
 var app = express();
+
+const upload = multer({ dest: "./public/data/uploads" })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +14,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    const { originalname, mimetype, size } = req.file;
+    res.json({
+        name: originalname,
+        type: mimetype,
+        size,
+    });
+});
 
 
 const port = process.env.PORT || 3000;
